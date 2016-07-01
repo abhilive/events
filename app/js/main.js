c87aabf70fc14d0043c9815f0cbed54cd87c1757
abhilive <cs.abhilive@gmail.com>
2016-07-01 14:49:53 +0530

    Fix Slider Issue

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,7 @@
  * Must include the dependency on 'ngMaterial'
  */
 var app = angular.module('SclApp', ['ngMaterial', 'ngAnimate', 'angular-flexslider', 'ngMessages', 'ui.router', 'material.svgAssetsCache']);
-
+//#DCDCC1
 app.config( function($mdThemingProvider) {  
     /*$mdThemingProvider.theme('default')
           .primaryPalette('blue')
@@ -237,6 +237,40 @@ app.controller('WhatshotCtrl', function ($mdDialog, $state, $scope, $rootScope, 
 
 app.controller('PicsCtrl', function ($mdDialog, $state, $stateParams, $scope, $rootScope, Data) {
   console.log('PicsCtrl reporting for duty.');
+
+  // Set of Photos : Start
+    $scope.photos = [
+        {src: 'http://farm9.staticflickr.com/8042/7918423710_e6dd168d7c_b.jpg', desc: 'Image 01'},
+        {src: 'http://farm9.staticflickr.com/8449/7918424278_4835c85e7a_b.jpg', desc: 'Image 02'},
+        {src: 'http://farm9.staticflickr.com/8457/7918424412_bb641455c7_b.jpg', desc: 'Image 03'},
+        {src: 'http://farm9.staticflickr.com/8179/7918424842_c79f7e345c_b.jpg', desc: 'Image 04'},
+        {src: 'http://farm9.staticflickr.com/8315/7918425138_b739f0df53_b.jpg', desc: 'Image 05'},
+        {src: 'http://farm9.staticflickr.com/8461/7918425364_fe6753aa75_b.jpg', desc: 'Image 06'}
+    ];
+
+    // initial image index
+    $scope._Index = 0;
+
+    // if a current image is the same as requested image
+    $scope.isActive = function (index) {
+        return $scope._Index === index;
+    };
+
+    // show prev image
+    $scope.showPrev = function () {
+        $scope._Index = ($scope._Index > 0) ? --$scope._Index : $scope.photos.length - 1;
+    };
+
+    // show next image
+    $scope.showNext = function () {
+        $scope._Index = ($scope._Index < $scope.photos.length - 1) ? ++$scope._Index : 0;
+    };
+
+    // show a certain image
+    $scope.showPhoto = function (index) {
+        $scope._Index = index;
+    };
+    /*END*/
 
   $scope.prod = {imagePaths: []};
   //$scope.prod = function() {
